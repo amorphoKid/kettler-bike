c62525d2d29ac5da640611f4a2dbcd423ece6676
joerg <you@example.com>
2022-12-31 19:02:14 +0100

    serial-interface

---
+++ learning/serial-interface/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to serial-interface
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ learning/serial-interface/test/serial_interface/core_test.clj
@@ -0,0 +1,7 @@
+(ns serial-interface.core-test
+  (:require [clojure.test :refer :all]
+            [serial-interface.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
