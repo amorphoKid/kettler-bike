44a55c86c07636cfe2f2b1d21a9179c20af158c5
joerg <you@example.com>
2022-12-31 19:05:45 +0100

    delete unwanted dir

--- learning/serial-killer/doc/intro.md
+++
@@ -1,3 +0,0 @@
-# Introduction to serial-killa
-
-TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
--- learning/serial-killer/test/serial_killa/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns serial-killa.core-test
-  (:require [clojure.test :refer :all]
-            [serial-killa.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
